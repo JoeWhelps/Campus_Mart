3f714caae69fb3da59f0839fe7ad7de7f1113d44
Joseph Whelpley <jwhelple@nd.edu>
2025-04-12 14:38:12 -0400

    login page

--- campusmart/marketplace/views.py
+++ campusmart/marketplace/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 def home(request):
-    return render(request, 'marketplace/templates/marketplace/home.html')
+    return render(request, 'marketplace/home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # After signup, go to login page
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
