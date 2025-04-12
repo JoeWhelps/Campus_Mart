147fbf29c4290e353f3fd8a7a6f91e81c46b9644
Kathleen Hurley <khurley6@student05.cse.nd.edu>
2025-04-12 15:42:10 -0400

    models

--- campusmart/marketplace/models.py
+++ campusmart/marketplace/models.py
@@ -6,3 +6,11 @@ class User(models.Model):
     username = models.CharField(max_length=200, unique=True)
     password = models.TextField()
     email = models.EmailField(max_length=200, unique=True)
+
+class Listing(models.Model):
+    title = models.CharField(max_length=200, unique=False)
+    description = models.CharField(max_length=500, unique=False)
+    price = models.IntegerField()
+    condition = models.CharField(max_length=100, unique=False)
+    status = models.Charfield(max_length=50, unique=False)
+    photo = models.ImageField(upload_to='images/')
