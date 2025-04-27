7a8b5127dfe1524bba25aeed3c17c89f9468b664
Kathleen Hurley <khurley6@student05.cse.nd.edu>
2025-04-27 16:42:12 -0400

    changes

--- campusmart/listings/models.py
+++ campusmart/listings/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.class 
+
+class User(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    username = models.CharField(max_length=200, unique=True)
+    password = models.TextField()
+    email = models.EmailField(max_length=200, unique=True)
+    
+class Listing(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    price = models.IntegerField()
+    condition = models.CharField(max_length=100)
+    status = models.CharField(max_length=50, default="Available")
+    photo = models.ImageField(upload_to='images/')
+    seller = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='listings')
--- campusmart/listings/views.py
+++ campusmart/listings/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Listing
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def create_listing(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        condition = request.POST.get('condition')
+        photo = request.FILES.get('photo')
+
+        if not (title and description and price and condition and photo):
+            return render(request, 'listings/create_listing.html', {'error': 'All fields are required.'})
+
+        # Create and save the new Listing
+        listing = Listing.objects.create(
+            title=title,
+            description=description,
+            price=price,
+            condition=condition,
+            photo=photo,
+            seller=request.user
+        )
+        return redirect('marketplace:listings')  
+
+    return render(request, 'listings/create_listing.html')
+
